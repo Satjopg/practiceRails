86362334fe10f43af240f197b08c10f644e3990b
Satjopg <sss.jopg@me.com>
2016-12-18 14:22:25 +0900

    edit routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,17 @@ class UsersController < ApplicationController
   def show
     # ハッシュ(連想配列)の生成
     @user = Hash.new { |hash, key| raise(IndexError, "user[#{key}] has no value")}
-    # :(文字列)はシンボル。文字列とは違い実際は数値として認識されている。(参照が文字列より早いらしい)
-    @user[:name] = "Satoru Murakami"
-    @user[:username] = "satoru"
-    @user[:location] = "IKUTA"
-    @user[:about] = "Hello, World!!"
+    if params[:username] == "satoru" then
+        # :(文字列)はシンボル。文字列とは違い実際は数値として認識されている。(参照が文字列より早いらしい)
+      @user[:name] = "Satoru Murakami"
+      @user[:username] = "satoru"
+      @user[:location] = "IKUTA"
+      @user[:about] = "Hello, World!!"
+    else
+      @user[:name] = "NO, NAME"
+      @user[:username] = params[:username]
+      @user[:location] = "NO LOCATION"
+      @user[:about] = "パラメーター取れたああ!!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/index'
-
-  get 'users/show'
-
+  # :~ でパラメータを取得. パラメーターはparamsに入る.
+  # to~ 指定したコントローラーに飛び, #~ ~の処理(アクション)をしろという意味
+  get 'users/show/:username', to: 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
