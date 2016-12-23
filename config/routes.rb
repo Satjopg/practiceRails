c79b15a2f1ae6ece3dd4084cbae07f249e034208
Satjopg <sss.jopg@me.com>
2016-12-23 18:15:59 +0900

    edit static page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'static_pages/help'
 
   get 'users/index'
+
+  get "users/show" , to: "users#show"
   # :~ でパラメータを取得. パラメーターはparamsに入る.
   # to~ 指定したコントローラーに飛び, #~ ~の処理(アクション)をしろという意味
   get 'users/show/:username', to: 'users#show'
