88e175b89f46309eac70948286b642f83c004650
Satjopg <sss.jopg@me.com>
2016-12-24 02:09:44 +0900

    edit helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = "First App"
+    if page_title.enpty? then
+      base_title
+    else
+      page_title + "|" + base_title
+    end
+  end
 end
