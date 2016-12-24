265557c615ec15833b666713b6b01974c191b490
Satjopg <sss.jopg@me.com>
2016-12-24 12:15:48 +0900

    edit apphelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
+# railsではHelperモジュールを自動的にinclude(:外部ファイルを使えるようにすること)してくれるので
+# わざわざrequireなどしなくても,　どこからでも呼び出しが可能
   def full_title(page_title = "")
     base_title = "First App"
-    if page_title.enpty? then
+    # empty?:オブジェクトは存在しているけどその中身の有無を判断
+    if page_title.empty?
+    # 暗黙の戻り値(returnなくても呼び出し元に返ってくれる...)
       base_title
     else
       page_title + "|" + base_title
